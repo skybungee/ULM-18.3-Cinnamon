599c714843cb53efac7ee947d796aa05c9099a5f
Skybungee <mv.bingham@gmail.com>
2018-06-23 13:43:19 -0400

    Comment : updated Brackets to 1.13 from 1.8 on Sat Jun 23 13:43:19 EDT 2018

--- install-brackets-v1.sh
+++ install-brackets-v1.sh
@@ -16,16 +16,16 @@
 sudo apt install -y libgcrypt11 libcurl3
 
 wget http://ftp.de.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u4_amd64.deb
-wget https://github.com/adobe/brackets/releases/download/release-1.8/Brackets.Release.1.8.64-bit.deb
-
+#wget https://github.com/adobe/brackets/releases/download/release-1.8/Brackets.Release.1.8.64-bit.deb
+wget https://github.com/adobe/brackets/releases/download/release-1.13/Brackets.Release.1.13.64-bit.deb
 
 sudo dpkg -i libgcrypt11_1.5.0-5+deb7u4_amd64.deb
-sudo dpkg -i Brackets.Release.1.8.64-bit.deb
+sudo dpkg -i Brackets.Release.1.13.64-bit.deb
 
 sudo apt-get -f install -y
 
 
-rm Brackets.Release.1.8.64-bit.deb
+rm Brackets.Release.1.13.64-bit.deb
 rm libgcrypt11_1.5.0-5+deb7u4_amd64.deb
 
 echo "################################################################"

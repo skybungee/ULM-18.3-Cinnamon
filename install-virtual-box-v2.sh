e40e623377f5347adaaf4179927a593a6f3d7366
Skybungee <mv.bingham@gmail.com>
2018-06-23 01:26:47 -0400

    Comment : updated Virtualbox installer on Sat Jun 23 01:26:47 EDT 2018

--- install-virtual-box-v2.sh
+++ install-virtual-box-v2.sh
@@ -16,8 +16,8 @@ rm /tmp/virtual-box.deb
 
 #wget http://download.virtualbox.org/virtualbox/5.1.12/virtualbox-5.1_5.1.12-112440~Ubuntu~xenial_amd64.deb -O /tmp/virtual-box.deb
 #wget http://download.virtualbox.org/virtualbox/5.1.14/virtualbox-5.1_5.1.14-112924~Ubuntu~xenial_amd64.deb -O /tmp/virtual-box.deb
-wget http://download.virtualbox.org/virtualbox/5.1.24/virtualbox-5.1_5.1.24-117012~Ubuntu~xenial_amd64.deb -O /tmp/virtual-box.deb
-
+#wget http://download.virtualbox.org/virtualbox/5.1.24/virtualbox-5.1_5.1.24-117012~Ubuntu~xenial_amd64.deb -O /tmp/virtual-box.deb
+wget http://download.virtualbox.org/virtualbox/5.2.12/virtualbox-5.2_5.2.12-122591~Ubuntu~xenial_amd64.deb -O /tmp/virtual-box.deb
 sudo dpkg -i /tmp/virtual-box.deb
 #gdebi /tmp/code_1.5.3-1474533365_amd64.deb
 

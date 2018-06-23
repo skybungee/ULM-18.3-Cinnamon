154490b657b20cd285c4a3d1983a0082aac00ea8
skybungee <mv.bingham@gmail.com>
2018-06-23 13:49:56 -0400

    Changed from version 1.0.0 to 1.4.4

--- More software/install-etcher-v1.sh
+++ More software/install-etcher-v1.sh
@@ -16,7 +16,8 @@ rm /tmp/etcher.zip
 
 # if there is no folder then make one
 [ -d /opt/Etcher ] || mkdir -p /opt/Etcher
-wget https://resin-production-downloads.s3.amazonaws.com/etcher/1.0.0-beta.17/Etcher-1.0.0-beta.17-linux-x64.zip -O /tmp/etcher.zip
+#wget https://resin-production-downloads.s3.amazonaws.com/etcher/1.0.0-beta.17/Etcher-1.0.0-beta.17-linux-x64.zip -O /tmp/etcher.zip
+wget https://github.com/resin-io/etcher/releases/download/v1.4.4/etcher-electron-1.4.4-linux-x64.zip -O /tmp/etcher.zip
 sudo unzip -o /tmp/etcher.zip -d /opt/Etcher
 
 rm /tmp/etcher.zip
